e895503873e163d9071bf11854669d2dcb6dd3ee
Nitin <getnpk@gmail.com>
2014-01-04 19:54:30 +0800

    bounds exception handling

--- src/com/getnpk/monkey/graphics/Screen.java
+++ src/com/getnpk/monkey/graphics/Screen.java
@@ -5,8 +5,9 @@ public class Screen {
 	private int width, height;
 
 	public int[] pixels;
-	int time = 0;
+	int xtime = 0, ytime = 0;
 	int counter=0;
+	
 	public Screen(int width, int height){
 		this.width = width;
 		this.height = height;
@@ -22,11 +23,14 @@ public class Screen {
 	
 	public void render(){
 		counter++;
-		if (counter % 10 == 0)
-			time++;
+		if (counter % 10 == 0) xtime++;
+		if (counter % 50 == 0) ytime++;
+
 		for (int y = 0; y < height; y++){
+			if (ytime >= height) break;
 			for (int x = 0; x < width; x++){
-				pixels[time+time*width] = 0xaf22af;
+				if (xtime >= width) break;
+				pixels[xtime + ytime*width] = 0xaf22af;
 			}
 		}
 	}
