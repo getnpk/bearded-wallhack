7f47c130105646f6bbc54e538067ebed4c8dbaf0
Nitin <getnpk@gmail.com>
2014-01-04 20:01:35 +0800

    outofbounds exp correction

--- src/com/getnpk/monkey/graphics/Screen.java
+++ src/com/getnpk/monkey/graphics/Screen.java
@@ -27,9 +27,9 @@ public class Screen {
 		if (counter % 50 == 0) ytime++;
 
 		for (int y = 0; y < height; y++){
-			if (ytime >= height) break;
+			if (ytime < 0 || ytime >= height) break;
 			for (int x = 0; x < width; x++){
-				if (xtime >= width) break;
+				if (xtime < 0 || xtime >= width) break;
 				pixels[xtime + ytime*width] = 0xaf22af;
 			}
 		}
