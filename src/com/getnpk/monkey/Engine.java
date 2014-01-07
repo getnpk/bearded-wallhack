4bcaee10a391918f4bd7d5ee2490cec8f5d6f389
unknown <nitinkp@sg01AA1NNitin.snshub.org>
2014-01-07 18:09:23 +0800

    key input and keylistener for canvas

--- src/com/getnpk/monkey/Engine.java
+++ src/com/getnpk/monkey/Engine.java
@@ -11,6 +11,7 @@ import java.awt.image.DataBufferInt;
 import javax.swing.JFrame;
 
 import com.getnpk.monkey.graphics.Screen;
+import com.getnpk.monkey.input.Keyboard;
 
 public class Engine extends Canvas implements Runnable {
 
@@ -25,6 +26,7 @@ public class Engine extends Canvas implements Runnable {
 	
 	private Thread thread;
 	private JFrame frame;
+	private Keyboard key;
 
 	private boolean running = false;
 
@@ -39,6 +41,9 @@ public class Engine extends Canvas implements Runnable {
 
 		screen = new Screen(width, height);
 		frame = new JFrame();
+		
+		key = new Keyboard();
+		addKeyListener(key);
 	}
 
 	public synchronized void start() {
@@ -95,6 +100,7 @@ public class Engine extends Canvas implements Runnable {
 	int x = 0, y = 0;
 	
 	public void update() {
+		key.update();
 		x++;
 		y++;
 	}
